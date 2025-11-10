ba0c0ec8406de76a4e0c45252538739e5877f545
angelostori <angelostori13@gmail.com>
2025-11-10 15:56:14 +0100

    set the project layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,26 @@ function App() {
 
   return (
     <>
-      <ul>
-        {articles.map(article =>
-          <li key={article.id}>{article.title}</li>
-        )}
-      </ul>
+      <div className="container">
+        <h3>Articoli:</h3>
+        <ul className='my-3'>
+          {articles.map(article =>
+            <li key={article.id}>{article.title}</li>
+          )}
+        </ul>
+      </div>
+
+
+      <div className="container">
+        <h4>Aggiungi nuovo articolo</h4>
+        <label class="form-label">Titolo:</label>
+        <input
+          type="text"
+          className="form-control"
+          required
+        />
+        <button className='btn btn-dark my-3' type="submit">Aggiungi</button>
+      </div>
 
     </>
   )
